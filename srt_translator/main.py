02f0efcc2a27721770732404d87500bffb7acb9b
chenchenick <chenchenick@gmail.com>
2024-10-03 10:04:17 -0700

    separate tranlate working, too slow

--- srt_translator/main.py
+++ srt_translator/main.py
@@ -1,7 +1,7 @@
 import os
 import json
 from .parser import parse_srt
-from .preprocessor import extract_texts, combine_texts, remove_overlaps
+from .preprocessor import extract_texts, remove_overlaps
 from .postprocessor import align_translations
 from .exporter import export_to_srt
 from translation_agent import translate
@@ -22,29 +22,28 @@ def translate_srt(input_srt_path, output_srt_path, source_lang, target_lang, cou
     texts = extract_texts(captions)
     save_step_result("2_extracted_texts", texts, output_dir)
     
-    combined_text = combine_texts(texts)
-    save_step_result("3_combined_text", {"combined_text": combined_text}, output_dir)
-    
-    cleaned_text = remove_overlaps(texts)
-    save_step_result("4_cleaned_text", {"cleaned_text": cleaned_text}, output_dir)
+    # Remove the combine_texts step
     
     print(f"Translating text...")
-    translated_text = translate(
-        source_lang=source_lang,
-        target_lang=target_lang,
-        source_text=cleaned_text,
-        country=country,
-    )
-    save_step_result("5_translated_text", {"translated_text": translated_text}, output_dir)
+    translated_texts = []
+    for text in texts:
+        translated_text = translate(
+            source_lang=source_lang,
+            target_lang=target_lang,
+            source_text=text,
+            country=country,
+        )
+        # Extract only the translation from the response
+        translation_start = translated_text.find("<TRANSLATION>") + len("<TRANSLATION>")
+        translation_end = translated_text.find("</TRANSLATION>")
+        if translation_start != -1 and translation_end != -1:
+            translated_text = translated_text[translation_start:translation_end].strip()
+        translated_texts.append(translated_text)
     
-    # Extract only the translation from the response
-    translation_start = translated_text.find("<TRANSLATION>") + len("<TRANSLATION>")
-    translation_end = translated_text.find("</TRANSLATION>")
-    if translation_start != -1 and translation_end != -1:
-        translated_text = translated_text[translation_start:translation_end].strip()
+    save_step_result("3_translated_texts", translated_texts, output_dir)
     
-    aligned_captions = align_translations(captions, translated_text)
-    save_step_result("6_aligned_captions", aligned_captions, output_dir)
+    aligned_captions = align_translations(captions, translated_texts)
+    save_step_result("4_aligned_captions", aligned_captions, output_dir)
     
     export_to_srt(aligned_captions, output_srt_path)
     print(f"Translation complete. Translated SRT saved to {output_srt_path}")
--- srt_translator/postprocessor.py
+++ srt_translator/postprocessor.py
@@ -1,26 +1,7 @@
-def align_translations(captions, translated_text):
-    total_original_length = sum(len(caption['text']) for caption in captions)
-    translated_text_length = len(translated_text)
+def align_translations(captions, translated_texts):
     aligned_captions = []
-    current_index = 0
-
-    for caption in captions:
-        original_length = len(caption['text'])
-        proportion = original_length / total_original_length
-        translated_length = int(proportion * translated_text_length)
-        translated_segment = translated_text[current_index:current_index + translated_length]
-        # Adjust to the nearest space to avoid cutting words
-        next_space = translated_text.find(' ', current_index + translated_length)
-        if next_space != -1:
-            translated_segment = translated_text[current_index:next_space]
-            current_index = next_space + 1
-        else:
-            current_index = translated_text_length
-
-        aligned_captions.append({
-            'sequence': caption['sequence'],
-            'start': caption['start'],
-            'end': caption['end'],
-            'text': translated_segment.strip()
-        })
+    for caption, translated_text in zip(captions, translated_texts):
+        aligned_caption = caption.copy()
+        aligned_caption['text'] = translated_text
+        aligned_captions.append(aligned_caption)
     return aligned_captions
